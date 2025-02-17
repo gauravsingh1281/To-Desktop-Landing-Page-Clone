bd161db9f0dac0ea299580f6e45ecfde26cf035e
Gaurav Pratap Singh <gs492979@gmail.com>
2025-02-17 22:24:06 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: ["Poppins", "serif"],
+        Inter: ["Inter", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
