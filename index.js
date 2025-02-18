94e364fea6c3647d6396fbdbe0732b91f6916862
Gaurav Pratap Singh <gs492979@gmail.com>
2025-02-18 23:15:12 +0530

    Companies name and scroll animation added.

--- index.js
+++ index.js
@@ -2,3 +2,37 @@ const mobileNav = document.getElementById("mobile-nav");
 function handleMenu() {
   mobileNav.classList.toggle("hidden");
 }
+const initialTranslateLTR = -48 * 4;
+const initialTranslateRTL = 36 * 4;
+
+function setupIntersectionObserver(element, isLTR, speed) {
+  const intersectionCallback = (enteries) => {
+    const isIntersecting = enteries[0].isIntersecting;
+    if (isIntersecting) {
+      document.addEventListener("scroll", scrollHandler);
+    } else {
+      document.removeEventListener("scroll", scrollHandler);
+    }
+  };
+  const intersectionObserver = new IntersectionObserver(intersectionCallback);
+  intersectionObserver.observe(element);
+  function scrollHandler() {
+    const translateX =
+      (window.innerHeight - element.getBoundingClientRect().top) * speed;
+    let totalTranslate = 0;
+    if (isLTR) {
+      totalTranslate = translateX + initialTranslateLTR;
+    } else {
+      totalTranslate = -(translateX + initialTranslateRTL);
+    }
+    element.style.transform = `translate(${totalTranslate}px)`;
+  }
+}
+
+const line1 = document.getElementById("line1");
+const line2 = document.getElementById("line2");
+const line3 = document.getElementById("line3");
+
+setupIntersectionObserver(line1, true, 0.25);
+setupIntersectionObserver(line2, false, 0.25);
+setupIntersectionObserver(line3, true, 0.25);
